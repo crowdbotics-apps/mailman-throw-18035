73d4e42c4bf3d82b5e2ae543fb1719cbab98e875
Crowdbotics <team@crowdbotics.com>
2020-06-12 04:40:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mailman_throw_18035/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mailman_throw_18035;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mailman_throw_18035";
+    }
+}
